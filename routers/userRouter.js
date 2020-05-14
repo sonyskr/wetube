0d10d3061ff4c446142158cdc7f8be3b038404ca
sonyskr <youngsik.son@gmail.com>
2020-05-13 21:20:09 -0700

    View templates added using Pug

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,18 @@
 import express from "express";
 import routes from "../routes";
+import {
+    users,
+    userDetail,
+    editProfile,
+    changePassword
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, (req, res) => res.send('Users'));
-userRouter.get(routes.userDetail, (req, res) => res.send('User Detail'));
-userRouter.get(routes.editProfile, (req, res) => res.send('Edit User Profile'));
-userRouter.get(routes.changePassword, (req, res) => res.send('Edit User Password'));
+userRouter.get(routes.users, users);
+userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail, userDetail);
 
 
 export default userRouter;

9f4ddf7054d6380b6999a974f4251c9c00b6e1d3
sonyskr <youngsik.son@gmail.com>
2020-05-20 19:54:34 -0700

    DB Models Added (Video, Comment)

---
+++ db.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL,
+{
+    useNewUrlParser:true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () =>
+{
+    console.log("Connected to Db");
+}
+
+const handleError = error =>
+{
+    console.log(`Error on DB Connection:${error}`);
+}
+
+db.once("open", handleOpen);
+db.on("error", handleError);
+
--- middlewares.js
+++ middlewares.js
@@ -4,5 +4,9 @@ export const localsMiddleware = (req, res, next) =>
 {
     res.locals.siteName = "WeTube";
     res.locals.routes = routes;
+    res.locals.user = {
+        isAuthenticated: true,
+        id: 1123123
+    }
     next();
 }

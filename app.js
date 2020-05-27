2a5b78a8aaed7ca5df4e41624da588b290a536c3
sonyskr <youngsik.son@gmail.com>
2020-05-26 19:47:31 -0700

    MongoStore and Middlewares modified

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ import morgan from "morgan";// morgan is used for loggin in middleware
 import helmet from "helmet";// helemt is used for security
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import passport from "passport";
+import mongoose from "mongoose";
+import MongoStore from "connect-mongo";
+import session from "express-session";
 import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 
+import "./passport";
+
 const app = express();
+
+const CokieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug");
@@ -19,6 +27,14 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan("dev"));
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: new CokieStore({mongooseConnection: mongoose.connection})
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(localsMiddleware);
 
